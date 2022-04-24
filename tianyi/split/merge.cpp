3511b178b0904ce9c76c793d7c65423e8b3b3229
ezrameow <1293883574@qq.com>
2022-04-24 12:15:45 +0800

    更新了洛天依音源 加入混响

--- tianyi/split/merge.cpp
+++ tianyi/split/merge.cpp
@@ -23,15 +23,18 @@ string tostr(int x){
 	ostringstream os; os<<x; return os.str();
 }
 
+const int id[]={0,1,10,11,12,13,14,15,16,17,18,19,2,20,21,22,23,24,25,26,27,28,29,3,30,31,4,5,6,7,8,9};
+
 int main(){
 	freopen("output.json","w",stdout);
 	printf("{\n");
-	for(int i=0;i<32;++i){
+	for(int j=0;j<32;++j){
+		int i=id[j];
 		int f_size=0; unsigned char *mp3=read_all((tostr(i)+".mp3").c_str(),f_size);
 		char *r=(char*)b64_encode(mp3,f_size);
 		printf("\"%d.mp3\":\"data:audio/mp3;base64,%s\"",i,r);
 		free(r); delete[] mp3;
-		if(i!=31) printf(",\n");
+		if(j!=31) printf(",\n");
 	}printf("\n}");
 	return 0;
 }
