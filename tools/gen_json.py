edcc50a15471e2d806f8f72fdb2a71effcc845a0
UnknownO <i@mr-wu.top>
2022-07-30 00:52:19 +0800

    json生成工具按照音频顺序生成

--- tools/gen_json.py
+++ tools/gen_json.py
@@ -5,7 +5,8 @@ filebasename="kagamine_len"
 result={}
 # 需要ffmpeg库
 # 转换未mp3格式主要为了压缩大小，以免文件过大
-for i in range(0,32):
+order=[0,1,10,11,12,13,14,15,16,17,18,19,2,20,21,22,23,24,25,26,27,28,29,3,30,31,4,5,6,7,8,9]
+for i in order:
     cmd = f"ffmpeg -i {filebasename}_{str(i+1).rjust(2,'0')}.wav {str(i)}.mp3"
     os.system(cmd)
     with open(f'{str(i)}.mp3', 'rb') as file_obj:
